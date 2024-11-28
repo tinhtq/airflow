8122f85d772905a64f50362798ffa569b2c7e380
tinhtq <truongquangtinh1997@gmail.com>
2024-11-28 13:45:29 +0700

    update

--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -5,6 +5,17 @@ import requests
 from airflow.hooks.base import BaseHook
 
 
+def _get_minio_client():
+    minio = BaseHook.get_connection("minio")
+    client = Minio(
+        endpoint=minio.extra_dejson["endpoint_url"].split("//")[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False,
+    )
+    return client
+
+
 def _get_stock_prices(url, symbol):
     api = BaseHook.get_connection("stock_api")
     url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
@@ -13,27 +24,23 @@ def _get_stock_prices(url, symbol):
     return json.dumps(response.json()["chart"]["result"][0])
 
 
-def _store_prices(stock):
-    minio = BaseHook.get_connection("minio")
-    client = Minio(
-        endpoint=minio.extra_dejson["endpoint_url"].split("//")[1],
-        access_key=minio.login,
-        secret_key=minio.password,
-        secure=False,
-    )
-    print(minio)
+def _store_prices(prices):
+    client = _get_minio_client()
+
+    prices = json.loads(prices)
     bucket_name = "stock-market"
     if not client.bucket_exists(bucket_name):
         client.make_bucket(bucket_name)
-    stock = json.loads(stock)
-    print(stock)
-    symbol = stock["meta"]["symbol"]
-    data = json.dumps(symbol, ensure_ascii=False).encode("utf-8")
+    symbol = prices["meta"]["symbol"]
+    data = json.dumps(prices, ensure_ascii=False).encode("utf8")
     objw = client.put_object(
-        bucket_name,
-        object_name=f"{symbol}/price.json",
+        bucket_name=bucket_name,
+        object_name=f"{symbol}/prices.json",
         data=BytesIO(data),
         length=len(data),
     )
-    print(objw.etag)
     return f"{objw.bucket_name}/{symbol}"
+
+
+def _get_formatted_csv(path):
+    return 0
